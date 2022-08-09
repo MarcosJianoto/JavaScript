f675a57aad414fbe8dddfa5779e6abff98dff1c1
MarcosJianoto <marcos_jianotoo@hotmail.com>
2022-08-09 09:19:37 -0300

    Commit

---
+++ 1-JavaScript/Iniciante/aula21/index.js
@@ -0,0 +1,66 @@
+/*
+
+Operadores de comparação
+> maior que
+< menor que
+>= maior que ou igual a 
+<= maior que ou igual a
+== igualdade
+===igualdade estrita
+!= diferente
+!==diferente estrito
+
+*/
+
+console.log("---------------------------------------------------");
+
+const comp = 10 > 5;
+console.log(comp);
+
+console.log("---------------------------------------------------");
+
+const tempo = 5 / 2;
+console.log(tempo < 10);
+console.log(tempo);
+
+console.log("---------------------------------------------------");
+
+const tempor = 10 >= 11;
+console.log(tempor);
+
+console.log("---------------------------------------------------");
+
+const tempora = 10 < 11;
+console.log(tempora);
+
+console.log("---------------------------------------------------");
+
+const gdshiug = 12 == 12;
+console.log(gdshiug);
+
+console.log("---------------------------------------------------");
+
+const gdshiu = 12 === "12";
+console.log(gdshiu);
+
+console.log("---------------------------------------------------");
+
+const num1 = 50;
+const num2 = 20;
+const comport = num1 <= num2;
+console.log(comport);
+
+console.log("---------------------------------------------------");
+
+const num3 = 10; //number
+const num4 = "10"; //string
+const calicatt = num3 === num4;
+console.log(calicatt);
+
+console.log("---------------------------------------------------");
+
+const num5 = 5321
+const num6 = 5321
+const ghsdiughsd = num5 !== num6
+console.log(ghsdiughsd)
+
